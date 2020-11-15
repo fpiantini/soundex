7805f9dea23588073c76ecc0b2ea72ac6748ba6f
Francesco Piantini <francesco.piantini@gmail.com>
2020-11-15 23:55:09 +0100

    added single character tests & implementation

--- test/soundextest.cpp
+++ test/soundextest.cpp
@@ -5,10 +5,28 @@
 
 using namespace soundex;
 
-class Soundex {
+class Soundex
+{
+public:
+    std::string encode(const std::string &word) const { return zeroPad(word); }
+private:
+    std::string zeroPad(const std::string &word) const { return word + "000"; }
+
 };
 
 TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord)
 {
     Soundex soundex;
+
+    // auto encoded = soundex.encode("A");
+    // ASSERT_THAT(encoded, testing::Eq("A"));
+
+    ASSERT_EQ(soundex.encode("A"), "A000");
+}
+
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
+{
+ Soundex soundex;
+
+ ASSERT_EQ(soundex.encode("I"), "I000");
 }
